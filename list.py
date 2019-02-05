de0f844c87700834114d5b7f63b1400eb6edf9b2
BEHROOZ SAYAH <beh.sayah@gmail.com>
2019-02-04 20:29:13 -0800

    Rejected Item

--- list.py
+++ list.py
@@ -1,17 +1,29 @@
+#
+# In this file we are working on list.
+#
+#
+#
+
 myUniqueList = []
+myRejecteList = []
 
 #This function just add the value, no mather what.
 def add(ele):
   myUniqueList.append(ele);
-
+  
+def addIfElementIsRejected(ele):
+  myRejecteList.append(ele)
 
 def addIfUnique(ele):
   index = 0
   while index < len(myUniqueList):
-    if (myUniqueList[index] == ele) :  return False
+    if (myUniqueList[index] == ele) :  
+      addIfElementIsRejected(ele)
+      return False
     index+=1
   myUniqueList.append(ele)
   return True
+
 
 
 #This line just add the lement to 
@@ -24,3 +36,4 @@ print(addIfUnique('ele-1'));
 # It should add the element and return TRUE
 print(addIfUnique('ele-2'));
 print(myUniqueList);
+print(myRejecteList);
