fb27f1da9de4324fd3634f8e8c1ddc8d3cc94ac8
BEHROOZ SAYAH <beh.sayah@gmail.com>
2019-02-14 19:33:14 -0800

    advance loop

--- loop-advance.py
+++ loop-advance.py
@@ -1,1 +1,40 @@
-row, column = os.popen('stty size', 'r').read().split()
+#
+#
+# Draw Play board
+#
+#
+#
+
+
+# Import OS Module For
+import os
+
+# Get available console row and column
+row, column = os.popen('stty size', 'r').read().split();
+print('ROW : ' + row)
+print('COLUMN : ' + column)
+
+
+
+
+def drawPlayBoard(row, column):
+  totalRow = row + (row - 1)
+  totalColumn = column + (column - 1)
+  for countRow in range(totalRow):
+    rowString = ''
+    for countColumn in range(totalColumn):
+      if countRow%2 == 0:
+        if countColumn%2 == 0:
+          rowString += ' '
+        else:
+          rowString += '|'
+      else:
+        rowString += '-'
+    print(rowString);
+
+
+# Draw Dynamic Play Board
+drawPlayBoard(4,5)
+
+# Covert the screen
+drawPlayBoard(int(row)/2, int(column)/2)
