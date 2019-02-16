3ea411ce6ba29ccfcdf8378d44363ffd794fc84e
BEHROOZ SAYAH <beh.sayah@gmail.com>
2019-02-15 21:23:16 -0800

    Dictionary & Set

--- dictionary-set.py
+++ dictionary-set.py
@@ -9,8 +9,8 @@
 # Hold singer"s data
 data = {}
 
-def setDate():{
-  data["artist"] = "Sample Singer"
+def setDate():
+  data["artist"] = "Sample Singer";
   data["song"] = "Sample Song"
   data["album"] = "Sample Album"
   data["year"] = 2019
@@ -20,13 +20,26 @@ def setDate():{
   data["location"] = "/Users/your-user/musics"
   data["description"] = "It is sample description for the song."
   data["web"] = "http://www.sample.com"
-}
 
 
-def printData(){
+
+def printData():
   for singProperty in data:
-    print("singProperty : " + data[singProperty]) 
-}
+    print(singProperty + " : " + str(data[singProperty])) 
+
+def isValidData():
+  while(True):
+    key = input("Enter property : ")
+    if key == '': 
+      break
+    value = input('Enter Value : ')
+    if(key in data and str(data[key]) == value):
+      print('TRUE')
+    else:
+      print('FALSE')
 
 setDate()
 printData()
+isValidData()
+
+
